acf1c4bf928ce83540a3c199a498d377eb2b5101
milan.horvath <Milan.Horvath@wolterskluwer.com>
2019-05-08 15:36:26 +0200

    add elements

--- module-3/HomePage.js
+++ module-3/HomePage.js
@@ -7,7 +7,16 @@ class HomePage extends Layout {
     constructor() {
         super('HomePage', 'https://epam.com', {css: 'body'});
         this.logo = new Element('Logo', {css: '.header__logo'});
-        super.addChildren(this.logo);
+        this.addChildren(this.logo);
+
+        this.rollOver = new Element('RollOver', {css: '.rollover-tiles-ui'});
+        this.addChildren(this.rollOver);
+
+        this.header = new Element('Header', {css: '.header__content'});
+        this.addChildren(this.header);
+
+        this.title = new Element('Title', {css: '.background-video__content .text'});
+        this.addChildren(this.title);
     }
 }
 
